3f054b1ca386ca0473a350b17bb0e2537e230778
Miguel Alves <mp96dev@gmail.com>
2019-01-17 00:17:12 -0200

    Métodos de Lista

--- Class/usuario.php
+++ Class/usuario.php
@@ -56,6 +56,40 @@ class Usuario{
         }
     }
 
+    public static function getList(){
+        $sql = new SQL();
+
+        return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+    }
+
+    public static function search($login){
+        $sql = new SQL();
+
+        return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+            ':SEARCH'=> '%'.$login.'%'
+        ));
+    }
+
+    public function login($login, $password){
+        $sql = new SQL();
+
+        $results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+            ":LOGIN"=>$login,
+            ":PASSWORD"=>$password
+        ));
+
+        if(count($results) > 0){
+            $row = $results[0];
+
+            $this->setIDUsuario($row['id']);
+            $this->setLoginUsuario($row['deslogin']);
+            $this->setSenhaUsuario($row['dessenha']);
+            $this->setDtCadastro(new DateTime($row['dt_cadastro']));
+        }else{
+            throw new Exception("Login e/ou senha inválidos!");
+        }
+    }
+
     public function __toString(){
         return json_encode(array(
             "id"=>$this->getIDUsuario(),
--- index.php
+++ index.php
@@ -7,9 +7,23 @@ $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);*/
 
-$root = new Usuario();
-$root->loadByID(3);
+//Carrega um usuário
+//$root = new Usuario();
+//$root->loadByID(3);
+//echo $root;
 
-echo $root;
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//Carrega uma lista de usuarios buscando pelo login
+//$search = Usuario::search("jo"); 
+//echo json_encode($search);
+
+//Carrega um usuario usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("Miguel","13456");
+
+echo $usuario;
 
 ?>
